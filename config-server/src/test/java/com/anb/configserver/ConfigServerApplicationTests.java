01b447a93de7a73ff2ddcaf9ff23b1b0b1c244f2
AbdulAzizKorai <korai266@gmail.com>
2022-05-24 10:51:29 +0300

    Config server project added

---
+++ config-server/src/test/java/com/anb/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anb.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
